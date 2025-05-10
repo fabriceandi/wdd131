b3f27b6f5f94b2441338fa5ccc62b49ab3afb058
fabriceandi <tojofenitraandriah@gmail.com>
2025-05-11 00:19:11 +0200

    Home page added

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+//Current year
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `&copy;<span class="highlight">${today.getFullYear()}</span>`;
+
+//Last modification
+let lastModif = new Date(document.lastModified);
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `<strong>Last Modification: </strong>: ${lastModif}`;
